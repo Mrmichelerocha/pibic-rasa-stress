d5c151afbebb5e9c36bacd4bbff213ddd0360714
heitorhenriques <heiitorheenrique@gmail.com>
2023-11-08 14:03:06 -0300

    adicionando o actions do rasa

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,47 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+import json
+from typing import Any, Text, Dict, List
+
+import requests
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+
+class action_call_jason_agent(Action):
+
+    def name(self) -> Text:
+        return "action_call_jason_agent"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        url = "http://127.0.0.1:8080/webhook"  # Substitua "seu_endereco_aqui" pelo endereço correto
+        # Dados da solicitação
+        data = {
+            "tracker": {
+                        "senderID": tracker.sender_id, 
+                        "slots": {"intentName": tracker.get_slot('intentName'), 
+                                "name": tracker.get_slot('name'), 
+                                "session_started_metadata": None},
+            }}
+                                                                                                                                   
+        # Converter dados para JSON
+        json_data = json.dumps(data)
+
+        # Definir cabeçalhos da solicitação
+        headers = {
+        "Content-Type": "application/json"
+        }
+
+        # Enviar solicitação POST
+        response = requests.post(url, data=json_data, headers=headers)
+        dispatcher.utter_message(text=response.json()['responses'][0]['text'])
+
+        return []
+
+
